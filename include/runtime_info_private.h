3dbe8bd5436ee0295a371c0c16bd67c4c28adbcd
Jinkun Jang <jinkun.jang@samsung.com>
2013-03-13 01:46:10 +0900

    Tizen 2.1 base

---
+++ include/runtime_info_private.h
@@ -0,0 +1,176 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __TIZEN_SYSTEM_RUNTIME_INFO_PRIVATE_H__
+#define __TIZEN_SYSTEM_RUNTIME_INFO_PRIVATE_H__
+
+#include <vconf.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef enum {
+	RUNTIME_INFO_DATA_TYPE_STRING,
+	RUNTIME_INFO_DATA_TYPE_INT,
+	RUNTIME_INFO_DATA_TYPE_DOUBLE,
+	RUNTIME_INFO_DATA_TYPE_BOOL
+} runtime_info_data_type_e;
+
+typedef union {
+	int i;
+	bool b;
+	double d;
+	char *s;
+} runtime_info_value_u;
+
+typedef runtime_info_value_u * runtime_info_value_h;
+
+typedef int (*runtime_info_func_get_value) (runtime_info_value_h value);
+typedef int (*runtime_info_func_set_event_cb) (void);
+typedef void (*runtime_info_func_unset_event_cb) (void);
+
+void runtime_info_updated(runtime_info_key_e key);
+
+int runtime_info_vconf_get_value_int(const char *vconf_key, int *value);
+int runtime_info_vconf_get_value_bool(const char *vconf_key, bool *value);
+int runtime_info_vconf_get_value_double(const char *vconf_key, double *value);
+int runtime_info_vconf_get_value_string(const char *vconf_key, char **value);
+
+int runtime_info_vconf_set_event_cb(const char *vconf_key, runtime_info_key_e runtime_info_key, int slot);
+void runtime_info_vconf_unset_event_cb(const char *vconf_key, int slot);
+
+int runtime_info_flightmode_get_value(runtime_info_value_h);
+int runtime_info_flightmode_set_event_cb(void);
+void runtime_info_flightmode_unset_event_cb(void);
+
+int runtime_info_audiojack_get_value(runtime_info_value_h);
+int runtime_info_audiojack_set_event_cb(void);
+void runtime_info_audiojack_unset_event_cb(void);
+
+int runtime_info_wifi_status_get_value(runtime_info_value_h);
+int runtime_info_wifi_status_set_event_cb(void);
+void runtime_info_wifi_status_unset_event_cb(void);
+
+int runtime_info_bt_enabled_get_value(runtime_info_value_h);
+int runtime_info_bt_enabled_set_event_cb(void);
+void runtime_info_bt_enabled_unset_event_cb(void);
+
+int runtime_info_wifi_hotspot_get_value(runtime_info_value_h);
+int runtime_info_wifi_hotspot_set_event_cb(void);
+void runtime_info_wifi_hotspot_unset_event_cb (void);
+
+int runtime_info_bt_hotspot_get_value(runtime_info_value_h);
+int runtime_info_bt_hotspot_set_event_cb(void);
+void runtime_info_bt_hotspot_unset_event_cb(void);
+
+int runtime_info_usb_hotspot_get_value(runtime_info_value_h);
+int runtime_info_usb_hotspot_set_event_cb(void);
+void runtime_info_usb_hotspot_unset_event_cb(void);
+
+int runtime_info_location_service_get_value(runtime_info_value_h);
+int runtime_info_location_service_set_event_cb(void);
+void runtime_info_location_service_unset_event_cb(void);
+
+int runtime_info_location_agps_get_value(runtime_info_value_h);
+int runtime_info_location_agps_set_event_cb(void);
+void runtime_info_location_agps_unset_event_cb(void);
+
+int runtime_info_location_network_get_value(runtime_info_value_h);
+int runtime_info_location_network_set_event_cb(void);
+void runtime_info_location_network_unset_event_cb(void);
+
+int runtime_info_location_sensor_get_value(runtime_info_value_h);
+int runtime_info_location_sensor_set_event_cb(void);
+void runtime_info_location_sensor_unset_event_cb(void);
+
+int runtime_info_packet_data_get_value(runtime_info_value_h);
+int runtime_info_packet_data_set_event_cb(void);
+void runtime_info_packet_data_unset_event_cb(void);
+
+int runtime_info_data_roaming_get_value(runtime_info_value_h);
+int runtime_info_data_roaming_set_event_cb(void);
+void runtime_info_data_roaming_unset_event_cb(void);
+
+int runtime_info_silent_mode_get_value(runtime_info_value_h);
+int runtime_info_silent_mode_set_event_cb(void);
+void runtime_info_silent_mode_unset_event_cb(void);
+
+int runtime_info_vibration_enabled_get_value(runtime_info_value_h);
+int runtime_info_vibration_enabled_set_event_cb(void);
+void runtime_info_vibration_enabled_unset_event_cb(void);
+
+int runtime_info_rotation_lock_enabled_get_value(runtime_info_value_h);
+
+int runtime_info_24hour_format_get_value(runtime_info_value_h);
+int runtime_info_24hour_format_set_event_cb(void);
+void runtime_info_24hour_format_unset_event_cb(void);
+
+int runtime_info_first_day_of_week_get_value(runtime_info_value_h);
+int runtime_info_first_day_of_week_set_event_cb(void);
+void runtime_info_first_day_of_week_unset_event_cb(void);
+
+int runtime_info_language_get_value(runtime_info_value_h);
+int runtime_info_language_set_event_cb(void);
+void runtime_info_language_unset_event_cb(void);
+
+int runtime_info_region_get_value(runtime_info_value_h);
+int runtime_info_region_set_event_cb(void);
+void runtime_info_region_unset_event_cb(void);
+
+int runtime_info_gps_status_get_value(runtime_info_value_h);
+int runtime_info_gps_status_set_event_cb(void);
+void runtime_info_gps_status_unset_event_cb(void);
+
+int runtime_info_battery_charging_get_value(runtime_info_value_h);
+int runtime_info_battery_charging_set_event_cb(void);
+void runtime_info_battery_charging_unset_event_cb(void);
+
+int runtime_info_tvout_connected_get_value(runtime_info_value_h value);
+int runtime_info_tvout_connected_set_event_cb();
+void runtime_info_tvout_connected_unset_event_cb();
+
+int runtime_info_audio_jack_status_get_value (runtime_info_value_h value);
+int runtime_info_audio_jack_status_set_event_cb ();
+void runtime_info_audio_jack_status_unset_event_cb();
+
+int runtime_info_sliding_keyboard_opened_get_value(runtime_info_value_h value);
+int runtime_info_sliding_keyboard_opened_set_event_cb();
+void runtime_info_sliding_keyboard_opened_unset_event_cb();
+
+int runtime_info_usb_connected_get_value(runtime_info_value_h value);
+int runtime_info_usb_connected_set_event_cb();
+void runtime_info_usb_connected_unset_event_cb();
+
+int runtime_info_charger_connected_get_value(runtime_info_value_h value);
+int runtime_info_charger_connected_set_event_cb();
+void runtime_info_charger_connected_unset_event_cb();
+
+int runtime_info_vibration_level_haptic_feedback_get_value(runtime_info_value_h value);
+int runtime_info_vibration_level_haptic_feedback_set_event_cb();
+void runtime_info_vibration_level_haptic_feedback_unset_event_cb();
+
+int runtime_info_auto_rotation_enabled_get_value(runtime_info_value_h);
+int runtime_info_auto_rotation_enabled_set_event_cb(void);
+void runtime_info_auto_rotation_enabled_unset_event_cb(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __TIZEN_SYSTEM_RUNTIME_INFO_PRIVATE_H__ */
---
+++ src/runtime_info_vconf.c
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <vconf.h>
+#include <dlog.h>
+
+#include <runtime_info.h>
+#include <runtime_info_private.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+
+#define LOG_TAG "CAPI_SYSTEM_RUNTIME_INFO"
+
+int runtime_info_vconf_get_value_int(const char *vconf_key, int *value)
+{
+	return vconf_get_int(vconf_key, value);
+}
+
+int runtime_info_vconf_get_value_bool(const char *vconf_key, bool *value)
+{
+	return vconf_get_bool(vconf_key, (int *)value);
+}
+
+int runtime_info_vconf_get_value_double(const char *vconf_key, double *value)
+{
+	return vconf_get_dbl(vconf_key, value);
+}
+
+int runtime_info_vconf_get_value_string(const char *vconf_key, char **value)
+{
+	char *str_value = NULL;
+
+	str_value = vconf_get_str(vconf_key);
+
+	if (str_value != NULL) {
+		*value = str_value;
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+typedef void (*runtime_info_vconf_event_cb)(keynode_t *node, void *event_data);
+
+static void runtime_info_vconf_event_cb0(keynode_t *node, void *event_data)
+{
+	if (node != NULL)
+		runtime_info_updated((runtime_info_key_e)event_data);
+}
+
+static void runtime_info_vconf_event_cb1(keynode_t *node, void *event_data)
+{
+	if (node != NULL)
+		runtime_info_updated((runtime_info_key_e)event_data);
+}
+
+static void runtime_info_vconf_event_cb2(keynode_t *node, void *event_data)
+{
+	if (node != NULL)
+		runtime_info_updated((runtime_info_key_e)event_data);
+}
+
+static void runtime_info_vconf_event_cb3(keynode_t *node, void *event_data)
+{
+	if (node != NULL)
+		runtime_info_updated((runtime_info_key_e)event_data);
+}
+
+static void runtime_info_vconf_event_cb4(keynode_t *node, void *event_data)
+{
+	if (node != NULL)
+		runtime_info_updated((runtime_info_key_e)event_data);
+}
+
+static runtime_info_vconf_event_cb runtime_info_vconf_get_event_cb_slot(int slot)
+{
+	switch (slot) {
+	case 0:
+		return runtime_info_vconf_event_cb0;
+
+	case 1:
+		return runtime_info_vconf_event_cb1;
+
+	case 2:
+		return runtime_info_vconf_event_cb2;
+
+	case 3:
+		return runtime_info_vconf_event_cb3;
+
+	case 4:
+		return runtime_info_vconf_event_cb4;
+
+	default:
+		return NULL;
+	}
+}
+
+int runtime_info_vconf_set_event_cb(const char *vconf_key, runtime_info_key_e runtime_info_key, int slot)
+{
+	runtime_info_vconf_event_cb vconf_event_cb;
+
+	vconf_event_cb = runtime_info_vconf_get_event_cb_slot(slot);
+
+	if (vconf_event_cb == NULL)
+		return RUNTIME_INFO_ERROR_IO_ERROR;
+
+	if (vconf_notify_key_changed(vconf_key, vconf_event_cb, (void *)runtime_info_key))
+		return RUNTIME_INFO_ERROR_IO_ERROR;
+
+	return RUNTIME_INFO_ERROR_NONE;
+}
+
+void runtime_info_vconf_unset_event_cb(const char *vconf_key, int slot)
+{
+	runtime_info_vconf_event_cb vconf_event_cb;
+
+	vconf_event_cb = runtime_info_vconf_get_event_cb_slot(slot);
+
+	if (vconf_event_cb != NULL)
+		vconf_ignore_key_changed(vconf_key, vconf_event_cb);
+}
