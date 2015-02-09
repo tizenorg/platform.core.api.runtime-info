6a77aaf0efb5dbd4eb839ec0960280ffaecb0db9
Jiyoung Yun <jy910.yun@samsung.com>
2015-02-09 15:53:15 +0900

    runtime-info: Merge Tizen 2.3 source code into tizen branch
    Details:
    https://developer.tizen.org/dev-guide/native/2.3.0/org.tizen.mobile.native.apireference/group__CAPI__SYSTEM__RUNTIME__INFO__MODULE.html
    
    Signed-off-by: Jiyoung Yun <jy910.yun@samsung.com>
    Change-Id: I0c49d941e426b1c6b1369bc15377b9bf65c81843

---
+++ doc/runtime_info_doc.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __TIZEN_SYSTEM_RUNTIME_INFO_DOC_H__
+#define __TIZEN_SYSTEM_RUNTIME_INFO_DOC_H__
+
+/**
+ * @ingroup CAPI_SYSTEM_FRAMEWORK
+ * @defgroup CAPI_SYSTEM_RUNTIME_INFO_MODULE Runtime information
+ * @brief  The Runtime Information API provides functions to obtain runtime information.
+ *
+ * @section CAPI_SYSTEM_RUNTIME_INFO_MODULE_HEADER Required Header
+ *   \#include <runtime_info.h>
+ *
+ * @section CAPI_SYSTEM_RUNTIME_INFO_MODULE_OVERVIEW Overview
+ * The Runtime Information API provides functions to obtain runtime information, containing miscellaneous system preference.
+ * The Runtime Information available is stored in key/value pairs, where there may be different data types for the value.
+ *
+ * The runtime_info_set_changed_cb() registers a callback function for a particular key, which will be invoked by the System Service if the state matching that key changes.
+*
+*/
+
+#endif /* __TIZEN_SYSTEM_RUNTIME_INFO_DOC_H__ */
